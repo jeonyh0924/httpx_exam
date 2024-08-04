04b12f0404e431c8f9f6b52756cc4146790b97a3
okpos <tf@okposui-MacBookPro.local>
2024-08-04 11:55:52 +0900

    텍스트 파일 내 URL을 일괄 비동기 처리

--- README.md
+++ README.md
@@ -1,26 +1,37 @@
 ### django command 에서 async로 httpx 요청 하는 데모 코드
 
 ### python env
+
 ```angular2html
 python 3.12.2
 ```
 
 ### packages
+
 ```angular2html
 requirementx.txt
 ```
 
-### command exam
+### command crawling
+
 ```angular2html
-./manage.py exam http://example.com
+python manage.py crawling
 ```
 
-### exam command path
+### crawling command path
 ```
 django-httpx/
     manage.py
     core/
         management/
             commands/
-                exam.py
+                crawling.py
 ```
+
+
+### 코드 설명
+
+1. `python manage.py crawling` 실행 시 코드경로
+    - `core/management/commands/crawling.py` 내 커멘드 클래스의 `crawling` 함수 실행
+2. 프로젝트 내 루트 경로의 `url_list.txt` 파일에 들어있는 URL 수집
+3. 이후 일괄 비동기 처리
--- core/management/commands/crawling.py
+++ core/management/commands/crawling.py
@@ -1,32 +1,41 @@
 import asyncio
 import httpx
+import os
+import logging
+
 from asgiref.sync import sync_to_async
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+from django.conf import settings
 
 from core.models import LogTable
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
     help = 'django command 에서 async로 httpx 요청 하는 데모 코드 작성 해주세요.'
 
-    def add_arguments(self, parser):
-        parser.add_argument('url', type=str)
-
     def handle(self, *args, **options):
-        url = options['url']
-
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.exam(url))
+        loop.run_until_complete(self.crawling())
 
-    async def exam(self, url):
-        """
-        어싱크 메서드를 만들고 해당 함수 호출 시 특정 이벤트를 만들면 된다.
-        """
+    async def crawling(self):
+        url_list_path = os.path.join(settings.BASE_DIR, 'url_list.txt')
+        if not os.path.exists(url_list_path):
+            self.stdout.write(f"File not found: {url_list_path}")
+            return
+
+        with open(url_list_path, 'r') as file:
+            urls = file.readlines()
+
         async with httpx.AsyncClient() as client:
             tasks = []
-            for index in range(40):
-                tasks.append(self.fetch_and_log(client, url, index))
+            for index, url in enumerate(urls):
+                url = url.strip()
+                if url:
+                    tasks.append(self.fetch_and_log(client, url, index))
             await asyncio.gather(*tasks)
 
     async def fetch_and_log(self, client, url, index):
@@ -43,5 +52,5 @@ class Command(BaseCommand):
             index=index
         )
 
-        # 요청 응답 결과 출력
-        self.stdout.write(f"Request {index}: {response.status_code}")
+        # 어싱크 메서드 내 요청 응답 결과 출력 함수
+        logger.info(f"URL : {url} : {response.status_code}, index >> {index}")
